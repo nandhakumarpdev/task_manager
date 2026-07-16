fa01f360d369a5ff9b85e99479bcaeb06ce8c7d9
nandhakumarpdev <nandhakumarp764@gmail.com>
2026-07-16 11:51:26 +0530

    bug fixxing

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -103,6 +103,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
+         'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
